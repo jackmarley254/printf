310c87beb0860b94818b42e5769a2ce4c947b190
jackmarley254 <engndiritu1@gmail.com>
2023-08-24 01:42:53 +0300

    Debugging my code

--- _printf.c
+++ _printf.c
@@ -13,45 +13,50 @@ int _printf(const char *format, ...)
 	int size = 0;
 	va_list args;
 
-	va_start(args, format);
-
-	while (*format)
+	if (format == NULL)
 	{
-		if (*format == '%')
+		return (-1);
+
+			va_start(args, format);
+
+		while (*format)
 		{
-			write(1, format, 1);
-			size++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
 			if (*format == '%')
 			{
 				write(1, format, 1);
 				size++;
 			}
-			else if (*format == 'c')
+			else
 			{
-				char c = va_arg(args, int);
-				write(1, &c, 1);
-				size++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-				int str_len = 0;
+				format++;
+				if (*format == '\0')
+					break;
+				if (*format == '%')
+				{
+					write(1, format, 1);
+					size++;
+				}
+				else if (*format == 'c')
+				{
+					char c = va_arg(args, int);
+					write(1, &c, 1);
+					size++;
+				}
+				else if (*format == 's')
+				{
+					char *str = va_arg(args, char *);
+					int str_len = 0;
 
-				while (str[str_len] != 0);
+					while (str[str_len] != 0);
 
-				str_len++;
-				write(1, str, str_len);
-				size += str_len;
+					str_len++;
+					write(1, str, str_len);
+					size += str_len;
+				}
 			}
+			format++;
 		}
-		format++;
+		va_end(args);
 	}
-	va_end(args);
-	return (size);
+		return (size);
 }
