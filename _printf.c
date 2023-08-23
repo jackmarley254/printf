f66008477c1fb381d8b56fe71551f7a45db1149c
jackmarley254 <engndiritu1@gmail.com>
2023-08-24 01:06:16 +0300

    Debugging my code

--- _printf.c
+++ _printf.c
@@ -12,7 +12,6 @@ int _printf(const char *format, ...)
 {
 	int size = 0;
 	va_list args;
-	char c;
 
 	va_start(args, format);
 
@@ -20,36 +19,37 @@ int _printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
+			write(1, format, 1);
+			size++;
+		}
+		else
+		{
 			format++;
-			if (*format == 'c')
+			if (*format == '\0')
+				break;
+			if (*format == '%')
 			{
-				c = (char)va_arg(args, int);
+				write(1, format, 1);
+				size++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
 				write(1, &c, 1);
 				size++;
 			}
 			else if (*format == 's')
 			{
 				char *str = va_arg(args, char *);
+				int str_len = 0;
 
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					size++;
-				}
-			}
-			else if (*format == '%')
-			{
-				write(1, "%", 1);
-				size++;
+				while (str[str_len] != 0 && '\0');
+
+				str_len++;
+				write(1, str, str_len);
+				size += str_len;
 			}
 		}
-		else
-		{
-			write(1, format, 1);
-			size++;
-		}
-
 		format++;
 	}
 	va_end(args);
