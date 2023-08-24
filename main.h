55e093568a754ce3161627c0cfff9724fc03790b
jackmarley254 <engndiritu1@gmail.com>
2023-08-24 06:26:05 +0300

    defining flag modifiers

--- main.h
+++ main.h
@@ -19,6 +19,18 @@ typedef struct flags
 	int hash;
 } flags_t;
 
+/* Flag Modifier Macros */
+#define PLUS 1
+#define SPACE 2
+#define HASH 4
+#define ZERO 8
+#define NEG 16
+#define PLUS_FLAG (flags & 1)
+#define SPACE_FLAG ((flags >> 1) & 1)
+#define HASH_FLAG ((flags >> 2) & 1)
+#define ZERO_FLAG ((flags >> 3) & 1)
+#define NEG_FLAG ((flags >> 4) & 1)
+
 /* utils.c */
 int _strlen(const char *);
 int print(char *);
